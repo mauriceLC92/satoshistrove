7da3f459cdfd399872115ae96bf408f4cee7d473
Maurice Le Cordier <mauricelecordier@gmail.com>
2020-11-23 08:51:16 +0200

    header worrking

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,9 @@ import { NetWorth } from "../components/NetWorth";
 export default function Home() {
   return (
     <div className="min-height: 100vh; flex flex-col justify-center items-center">
-      <header className="bg-orange-700 h-10 w-full">
-        <nav>
-          <div className="bg-orange-400 text-pink-500">Item</div>
+      <header className="bg-blue-300 border-gray-800 border-b border-solid w-full">
+        <nav className="flex items-center h-10 px-5">
+          {/* <div className="">Item</div> */}
         </nav>
       </header>
       <main className="flex flex-col justify-center items-center px-5 py-5 space-y-10 space-y-24">
